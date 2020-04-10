3b8a56004473add38d7ff7773a06642ac3147f1e
greenhornsr <srollins11279@gmail.com>
2020-04-10 11:02:03 -0700

    initial commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,12 +8,44 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    results = []
+    
+    i = 0
+    for weight in weights:
+        hash_table_insert(ht, weight, i)
+        i += 1
+        results.append(limit-weight)
 
-    """
-    YOUR CODE HERE
-    """
+    check_HT_storage(ht)
 
+    for i in ht.storage:
+        if i is not None:
+            node = i
+            result = limit-node.key
+            # print("node.key is: ", node.key)
+            # print(f"result of {limit}-{node.key} is: ", result)
+            answer = hash_table_retrieve(ht, result)
+            if answer is not None:
+                # print("result: ", result)
+                # print("answer: ", answer, node.value)
+                # print("node.key: ", node.key)
+                # print("node.value: ", node.value)
+                low = min(result, node.key)
+                high = max(result, node.key)
+                # print("low: ", low, ", high: ", high)
+                return (high, low)
+            # else: 
+            #     print("no luck, trying again.")
     return None
+
+def check_HT_storage(ht):
+    for i in ht.storage:
+        if i is not None:
+            node = i
+            print("KEY - Post Insert ht Storage:  ", node.key)
+            print("VALUE - Post Insert ht Storage:  ", node.value)
+        else: 
+            print("this is HT storage: ", i)
 
 
 def print_answer(answer):
@@ -21,3 +53,11 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+# weights = [ 4, 6, 10, 15, 16 ]
+weights = [ 2, 4, 4, 6, 10, 15, 16, 20 ]
+length = 5
+limit = 21
+
+get_indices_of_item_weights(weights, length, limit)
